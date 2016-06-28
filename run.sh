24c663781eb4377a8ad57d95493ef2789884b3e3
haraldronge <harald@Haralds-MBP.fritz.box>
2016-06-28 15:38:52 +0200

    commentaar

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --allow-file-access-from-files file:///Users/harald/Documents/simple-web-worker/index.html
+#file Pfad anpassen Robert!
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --allow-file-access-from-files /Users/harald/Documents/robert_primzahlen/index.html
